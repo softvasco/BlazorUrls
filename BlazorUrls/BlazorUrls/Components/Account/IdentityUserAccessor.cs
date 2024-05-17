30d5fbdadf4320f5dc977d608aa13f41e3b561cc
softvasco <sprvasco@hotmail.com>
2024-05-17 16:43:51 +0100

    Add project files.

---
+++ BlazorUrls/BlazorUrls/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using BlazorUrls.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorUrls.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
